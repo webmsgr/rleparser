c00b3b90115389239e8103c8dbf7bd6ae26c6e65
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-12-04 12:02:09 -0800

    Update rleparser.py

--- rleparser/rleparser.py
+++ rleparser/rleparser.py
@@ -5,7 +5,7 @@ class RLEParser:
         self.input = []
         self.complete = False
     def parse(self,inp):
-        self.input = inp.split("\n")
+        self.input = inp.strip().split("\n")
         hasmet = False
         for line in self.input:
             if line.strip()[0] == "#":
@@ -16,4 +16,9 @@ class RLEParser:
                 else:
                     hasmet = True
                     print("Header: {}".format(line))
-     
+def main():
+    data = open("glider.rle").read()
+    parser.RLEParser()
+    parser.parse(data)
+if __name__ == "__main__":
+    main()
