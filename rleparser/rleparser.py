089477a8a32702bae1e4da170b2b6e71ee0b07ca
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-12-04 11:57:09 -0800

    Update rleparser.py

--- rleparser/rleparser.py
+++ rleparser/rleparser.py
@@ -1,3 +1,19 @@
 
-def parsestring(inp):
-    pass
+class RLEParser:
+    def __init__(self):
+        self.parsed = []
+        self.input = []
+        self.complete = False
+    def parse(self,inp):
+        self.input = inp.split("\n")
+        hasmet = False
+        for line in self.input:
+            if line.strip()[0] == "#":
+                print("Tag line: {}".format(line))
+            else:
+                if hasmet:
+                    print("Data line: {}".format(line))
+                else:
+                    hasmet = True
+                    print("Header: {}".format(line))
+     
